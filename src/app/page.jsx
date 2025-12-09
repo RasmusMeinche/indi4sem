526b3d2488b7fbcf38f726704d8db6d6078baa7f
Jesper <jefy0001@stud.kea.dk>
2025-12-09 13:54:48 +0100

    Refactor code structure for improved readability and maintainability

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,10 +4,15 @@ import Branchecard from "./components/byggebranchecard";
 import ProjektCard from "./projekter/page";
 import Cementsektion from "./sektioner/cementliste";
 import Cementtal from "./sektioner/cementtal";
+import Herosection from "./sektioner/herosection";
 
 export default async function Home() {
   return (
     <>
+      <Herosection
+        bgUrl="/stor_logo_hero.png"
+        height="h-[70vh]"
+      />
       <Branchecard />
       <Cementsektion />
       <Katalog />
