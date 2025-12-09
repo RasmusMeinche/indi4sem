94e7395c4cae6a50051ec08a653d0a31d733330b
Jesper <jefy0001@stud.kea.dk>
2025-12-09 14:34:22 +0100

    Add HeroSection to Partner and Projekt components for improved layout

--- src/app/partner/page.jsx
+++ src/app/partner/page.jsx
@@ -1,33 +1,36 @@
 import HeroSection from "../sektioner/herosection";
 import Karrusel from "../components/karrusel";
-import Cementliste from '../components/cementliste'
-import Sektionsoverskrift from '../components/sektionsoverskrift'
+import Cementliste from "../components/cementliste";
+import Sektionsoverskrift from "../components/sektionsoverskrift";
 
-export default function Partner () {
+export default function Partner() {
+  const tekst1 = (
+    <>
+      <strong>Fokus på bæredygthed:</strong> Vi prioriterer miljøløsninger i
+      alle vores projekter
+    </>
+  );
+  const tekst2 = (
+    <>
+      <strong>Stærke partnerskaber:</strong> Samarbejde er nøglen til vores
+      innovation og udvikling
+    </>
+  );
+  const tekst3 = (
+    <>
+      <strong>Målbare resultater:</strong> Vi leverer dokumenterede resultater
+      og konsekvensanalyser
+    </>
+  );
 
-const tekst1 = (
-  <>
-    <strong>Fokus på bæredygthed:</strong> Vi prioriterer miljøløsninger i alle vores projekter
-  </>
-);
-const tekst2 = (
-  <>
-    <strong>Stærke partnerskaber:</strong> Samarbejde er nøglen til vores innovation og udvikling
-  </>
-);
-const tekst3 = (
-  <>
-    <strong>Målbare resultater:</strong> Vi leverer dokumenterede resultater og konsekvensanalyser
-  </>
-);
-
-    return (
-        <>
-        < HeroSection 
-        bgUrl="/partner_hvid.png"
+  return (
+    <>
+      <HeroSection
+        bgUrl="/partner_hero.png"
         height="h-[60vh]"
-        />
-        <div className="
+      />
+      <div
+        className="
             bg-[url('/cementsektion.jpg')]
             bg-cover bg-center relative 
             before:content-[''] 
@@ -35,23 +38,25 @@ const tekst3 = (
             before:inset-0 
             before:bg-white/80
             py-(--content-padding)
-            ">
-            <div className='z-1 relative'>
-            < Sektionsoverskrift titel="Fordele partnerskab"/>
-            <Cementliste 
-            gridCols = "grid-cols-[0.3fr_2fr_0.3fr]"
-            tal1=''
-            tal2=''
-            tal3=''
+            "
+      >
+        <div className="z-1 relative">
+          <Sektionsoverskrift titel="Fordele partnerskab" />
+          <Cementliste
+            gridCols="grid-cols-[0.3fr_2fr_0.3fr]"
+            tal1=""
+            tal2=""
+            tal3=""
             tekst1={tekst1}
             tekst2={tekst2}
-            tekst3={tekst3}/>
-            </div>
-            <div></div>
+            tekst3={tekst3}
+          />
         </div>
-        <section className="py-(--content-padding)">
-            < Karrusel />
-        </section>
-        </>
-    )
+        <div></div>
+      </div>
+      <section className="py-(--content-padding)">
+        <Karrusel />
+      </section>
+    </>
+  );
 }
--- src/app/projekter/page.jsx
+++ src/app/projekter/page.jsx
@@ -1,35 +1,42 @@
 import Glasscard from "../components/glasscard";
+import Herosection from "../sektioner/herosection";
 import { getDataAlleArtikler } from "@/src/lib/fetching";
 import Sektionsoverskrift from "../components/sektionsoverskrift";
 import Link from "next/link";
 
-export default async function Projekt () {
+export default async function Projekt() {
+  const projekterData = await getDataAlleArtikler();
 
-    const projekterData = await getDataAlleArtikler();
-
-    return (
-        <section className=" w-(--content-size) py-(--content-padding) m-auto">
-            < Sektionsoverskrift titel="Projekter" />
-            <div className="grid grid-cols-3 justify-between gap-8">
-            {projekterData.map((item) => (
-                <Link key={item.id} href={`/projekter/${item.id}`}>
-                    <Glasscard
-                        width="full"
-                        object="object-cover"
-                        titleSize="text-xl"
-                        imgWidth="w-full"
-                        imgHeight="h-[180px]"
-                        margin="my-4"
-                        textWidth="w-full"
-
-                        overskrift={item.projekt}
-                        img={item.img}
-                        tekst={item.korttekst}
-                    />
-                </Link>
-            ))
-        }
+  return (
+    <>
+      <Herosection
+        bgUrl="/projekter_hero.png"
+        height="h-[60vh]"
+      />
+      <section className=" w-(--content-size) py-(--content-padding) m-auto">
+        <Sektionsoverskrift titel="Projekter" />
+        <div className="grid grid-cols-3 justify-between gap-8">
+          {projekterData.map((item) => (
+            <Link
+              key={item.id}
+              href={`/projekter/${item.id}`}
+            >
+              <Glasscard
+                width="full"
+                object="object-cover"
+                titleSize="text-xl"
+                imgWidth="w-full"
+                imgHeight="h-[180px]"
+                margin="my-4"
+                textWidth="w-full"
+                overskrift={item.projekt}
+                img={item.img}
+                tekst={item.korttekst}
+              />
+            </Link>
+          ))}
         </div>
-        </section>
-        )
+      </section>
+    </>
+  );
 }
