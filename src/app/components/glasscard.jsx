dc3f2a200b26add438eb5d79060fdc3e2d53567e
Jesper <jefy0001@stud.kea.dk>
2025-12-11 12:14:53 +0100

    Refactor Glass component for improved readability and consistency

--- src/app/components/glasscard.jsx
+++ src/app/components/glasscard.jsx
@@ -13,16 +13,16 @@ export default function Glass({
   titleSize = "sm:text-4xl",
   textWidth = "w-4/5",
   cardTekstSize = "sm:text-lg",
-  textOnButton = "Læs Mere"
+  textOnButton = "Læs Mere",
+  pdfUrl,
 }) {
   return (
     <div className={`${width} glass-card flex flex-col ${height} m-auto`}>
-      <div className={`text-center flex flex-col h-full justify-between *:my-1 md:*:my-4 [&_*:last-child]:mt-4`}>
-
+      <div
+        className={`text-center flex flex-col h-full justify-between *:my-1 md:*:my-4 [&_*:last-child]:mt-4`}
+      >
         {overskrift && (
-          <h1 className={`${titleSize} font-(--font-weight) `}>
-            {overskrift}
-          </h1>
+          <h1 className={`${titleSize} font-(--font-weight) `}>{overskrift}</h1>
         )}
 
         {img && (
@@ -33,7 +33,11 @@ export default function Glass({
         )}
 
         {tekst && (
-          <p className={`m-auto font-medium text-xs ${cardTekstSize} ${textWidth} ${textSize}`}>{tekst}</p>
+          <p
+            className={`m-auto font-medium text-xs ${cardTekstSize} ${textWidth} ${textSize}`}
+          >
+            {tekst}
+          </p>
         )}
 
         {textOnButton &&
