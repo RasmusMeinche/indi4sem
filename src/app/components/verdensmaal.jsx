88e2a32798d52d4a2d4a4bdb6fbfd55cc430cb84
Rasmus <rame0001@stud.kea.dk>
2025-12-11 12:30:32 +0100

    Update grid layout and styling for improved responsiveness in Ansvar and Verdensmaal components

--- src/app/components/verdensmaal.jsx
+++ src/app/components/verdensmaal.jsx
@@ -5,7 +5,7 @@ export default function Verdensmaal () {
         <section className='py-(--content-padding) bg-[url(/jordfarvet-bg.png)] bg-cover bg-center'>
                 < Sektionsoverskrift titel="FN’S Verdensmål"/>
                 <div className=" w-(--content-size) m-auto">
-                    <div className='glass-card [&_div]:gap-8 md:[&_div]:flex [&_img]:w-[150px] md:[&_img]:w-[200px] [&_img]:object-contain [&_img]:h-auto [&_img]:rounded-lg font-medium text-sm md:text-xl grid gap-8 first:p-8'>
+                    <div className='glass-card [&_div]:gap-8 md:[&_div]:flex [&_img]:mb-4 md:[&_img]:mb-0 text-center md:text-left [&_img]:w-[150px] md:[&_img]:w-[200px] [&_img]:object-contain [&_img]:h-auto [&_img]:m-auto [&_img]:rounded-lg font-medium text-sm md:text-md lg:text-lg xl:text-xl grid gap-8 first:p-8'>
                         <div>
                             <img src="/icon-9.jpg" alt="FN's Verdensmål" />
                             <p>Mål 9.4 (Bæredygtig infrastruktur og industri): Vi reducerer miljøpåvirkningen fra byggeri ved at genanvende betonaffald i stedet for at producere nyt beton, hvilket sparer ressourcer og mindsker CO₂-udledningen. Vores produkt er et konkret eksempel på, hvordan byggeindustrien kan overgå til mere ressourceeffektive og miljøvenlige løsninger. Herunder vises en tabel for CO2 udledningen af vores produkt i genbrugsbeton kontra konventionelle cykelstativer.</p>
