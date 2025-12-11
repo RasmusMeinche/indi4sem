8cd90d72d85e9d50f3cbf67d8204eb03a45ad855
Rasmus <rame0001@stud.kea.dk>
2025-12-11 14:38:57 +0100

    Enhance layout responsiveness in maalsaetning and sektionsoverskrift components

--- src/app/components/maalsaetning.jsx
+++ src/app/components/maalsaetning.jsx
@@ -14,7 +14,7 @@ export default function maalsaetning () {
                 ">
                 <div className="relative z-2 md:w-(--content-size) m-auto">
                     < Sektionsoverskrift titel="Målsætning"/>
-                    <div className="grid md:grid-cols-2 md:border-black md:border-b [&_h1]:font-bold [&_h1]:mb-4 [&_h1]:text-3xl [&_p]:font-medium [&_p]:text-xl [&_div]:p-8 text-center w-full h-fit">
+                    <div className="grid md:grid-cols-2 md:border-black md:border-b [&_h1]:font-bold [&_h1]:mb-4 [&_h1]:text-xl md:[&_h1]:text-3xl [&_p]:font-medium [&_p]:text-lg [&_div]:p-4 md:[&_div]:p-8 text-center w-full h-fit">
                         <div className="w-full h-full md:border-black md:border-r grid grid-rows-[1fr_3fr]">
                             <h1>Vision</h1>
                             <p>At skabe en grønnere fremtid gennem innovative løsninger, hvor afflad fra byggebranchen forvandles til funktionelle og inspirerende rum. Vi stræber efter at fremme cirkulær økonomi og bæredygtighed som grundstenene i vores byrumsdesign.</p>
