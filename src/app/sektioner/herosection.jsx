0e4c05df044085de758486f8ec75fee4c1e8445b
Rasmus <rame0001@stud.kea.dk>
2025-12-12 12:31:27 +0100

    Update on herosection with SVG repsonsiveness

--- src/app/sektioner/herosection.jsx
+++ src/app/sektioner/herosection.jsx
@@ -1,20 +1,11 @@
-export default function HeroSection({
-  bgUrl = "/forside_hero.png",
-  height = "h-[600px]",
-}) {
+export default function HeroSection() {
   return (
-    <section>
-      <div
-        className={`
-          relative 
-          bg-cover 
-          bg-center 
-          ${height}
-        `}
-        style={{
-          backgroundImage: `url(${bgUrl})`,
-        }}
-      ></div>
+    <section className="bg-[url('/optimeret_hero_bg.png')] bg-cover bg-center w-full p-12 sm:p-(--content-padding) sm:h-[90vh] relative flex items-center justify-center">
+      <img
+        className="w-full h-full object-contain"
+        src="/k-logo.svg"
+        alt="konkret logo"
+      />
     </section>
   );
 }
