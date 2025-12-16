cdaf36cecf2e548e4592bd122ca85458facc0ef9
Rasmus <rame0001@stud.kea.dk>
2025-12-16 23:26:10 +0100

    optimization and convertion from png to webp

--- src/app/sektioner/herosection.jsx
+++ src/app/sektioner/herosection.jsx
@@ -8,7 +8,7 @@ export default function HeroSection( {src="/k-logo.svg", vh="sm:h-[90vh]", overs
 
 
   return (
-    <section className={`bg-[url('/optimeret_hero_bg.png')] bg-cover bg-center w-full p-12 sm:p-(--content-padding) ${vh} relative flex items-center justify-center`}>
+    <section className={`bg-[url('/optimeret_hero_bg.webp')] bg-cover bg-center w-full p-12 sm:p-(--content-padding) ${vh} relative flex items-center justify-center`}>
       {src && (
       <img
         className="w-full h-full object-contain"
